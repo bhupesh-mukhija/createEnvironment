ae0d2f6e8e02e8cf58df528ca845673b9d824e83
bmukhija <creativecloudhead@gmail.com>
2020-02-26 13:53:00 +0000

    Update createEnv.sh

--- createEnv.sh
+++ createEnv.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sfdx:force:org:create -a testOrg -v sagegroup -f ../config/projet-scratch-def.json
+sfdx:force:org:create -a testOrg -v sagegroup -f config/scratch-org-config/projet-scratch-def.json
