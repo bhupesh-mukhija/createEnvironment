d32d6d254992a7f61b6805b267ee13f85d99bb87
bhupesh-sage <bhupesh.mukhija@sage.com>
2020-03-25 15:06:54 +0000

    Commit 1

--- createEnv.sh
+++ createEnv.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sfdx:force:org:create -a testOrg -v sagegroup -f config/scratch-org-config/projet-scratch-def.json
+sfdx force:org:list --all
+sfdx force:org:create -a testOrg -v sagegroup -f config/scratch-org-config/projet-scratch-def.json
